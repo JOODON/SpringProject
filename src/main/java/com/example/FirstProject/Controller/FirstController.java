fc39c3738944ecda01473355bb4dfd60209ae639
주동호 <100278301+JOODON@users.noreply.github.com>
2022-07-09 12:09:39 +0900

    네비게이션 바 제작 그리고 From 페이지 만들어주어야됨 !

--- src/main/java/com/example/FirstProject/Controller/FirstController.java
+++ src/main/java/com/example/FirstProject/Controller/FirstController.java
@@ -9,8 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class FirstController {
     @GetMapping("/HelloUser")
     public String Hi(Model model){
-        model.addAttribute("Username","사용자");
+        model.addAttribute("Username","동호");
         return "greetings";//브라우저로 전송시켜줌
+    }
+    @GetMapping("/bye")
+    public String bye(Model model){
+        model.addAttribute("nickname","동호");
+        return "goodbye";//브라우저로 전송시켜줌
     }
 }
 //모델이라는 프레임 워크를 가져옴
