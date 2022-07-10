2db15f9a6c68df40124816ddd8785223fed40833
주동호 <100278301+JOODON@users.noreply.github.com>
2022-07-11 01:06:18 +0900

    메인페이지 대충 제작 완료 이제 폼 데이터 전송

---
+++ src/main/resources/templates/main.js
@@ -0,0 +1,12 @@
+﻿$(function (){
+    $(`.Contact`).click(function (){
+        $(`.Board`).css({display:`block`}),
+            $(`.h2`).css({display:`none`});
+
+        $(`.Search`).click(function (){
+            $(`.Board`).css({display:`none`},
+                $(`.h2`).css({display:`block`}));
+        })
+    })
+})
+
